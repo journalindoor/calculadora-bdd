6f1fcb4a165cb5020dd6445f80dd3d77e4e5dc11
RSMJr <journal.indoor@gmail.com>
2020-05-17 19:39:20 -0300

    [140] Implementada a função de multiplicar

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,34 +1,38 @@
 var Calculadora = {
-    adicionar: function(num1, num2){
-        if(isNaN(num1) || isNaN(num2)) {
+    adicionar: function (num1, num2) {
+        if (isNaN(num1) || isNaN(num2)) {
             return 0;
         }
 
         num1 = parseFloat(num1);
         num2 = parseFloat(num2);
-        
+
         return num1 + num2;
     },
-    subtrair: function(num1, num2){
-        if(isNaN(num1) || isNaN(num2)){
+    subtrair: function (num1, num2) {
+        if (isNaN(num1) || isNaN(num2)) {
             return 0;
         }
 
         return num1 - num2;
     },
-    dividir: function(num1, num2){
-        if(num2 == 0){
+    dividir: function (num1, num2) {
+        if (num2 == 0) {
             return "Erro";
         }
 
-        if(isNaN(num1) || isNaN(num2)){
+        if (isNaN(num1) || isNaN(num2)) {
             return 0;
         }
 
         return num1 / num2;
     },
-    multiplicar: function(num1, num2){
-        return 0;
+    multiplicar: function (num1, num2) {
+        if (isNaN(num1) || isNaN(num2)) {
+            return 0;
+        }
+
+        return num1 * num2;
     },
 };
 // usado na função require do NodeJS
