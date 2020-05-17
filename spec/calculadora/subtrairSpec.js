5483a5722799226c3d6dc0e3621f5f978323ab67
RSMJr <journal.indoor@gmail.com>
2020-05-17 19:12:56 -0300

    [134] Implementado função subtrair

---
+++ spec/calculadora/subtrairSpec.js
@@ -0,0 +1,26 @@
+describe("Suite de testes de subtração", () => {
+
+    var Calculadora = require('../../src/js/calculadora.js');
+
+    it('deve retornar 5 para 8 e 3', () => {
+        expect(Calculadora.subtrair(8, 3)).toEqual(5);
+    });
+
+    it('deve retornar 5 para 8 e 3 em formato texto', () => {
+        expect(Calculadora.subtrair('8', '3')).toEqual(5);
+    });
+
+    it('deve retornar 4 para 5.5 e 1.5', () => {
+        expect(Calculadora.subtrair(5.5, 1.5)).toEqual(4);
+        expect(Calculadora.subtrair('5.5', '1.5')).toEqual(4);
+    });
+
+    it('deve retornar 0 para valor 1 invalido', () => {
+        expect(Calculadora.subtrair(undefined, 5)).toEqual(0);
+    });
+
+    it('deve retornar 0 para valor 2 invalido', () => {
+        expect(Calculadora.subtrair(5, undefined)).toEqual(0);
+    });
+
+});
